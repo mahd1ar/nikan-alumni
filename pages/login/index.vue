cafccffc8c4b0a4c65fde6194c1f080a972e9bb4
mahdiyar <a.mahdiyar7@live.com>
2024-01-30 10:49:39 +0330

    remove redirect to nikpeyvand

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -89,11 +89,6 @@ export default Vue.extend({
       loaded: false,
     }
   },
-
-  asyncData(args) {
-    args.redirect('https://nikan.nikpeyvand.com/login')
-  },
-
   computed: {
     action() {
       const action = this.$route.query.action as string | null
